80ada9aa9fba064c5a30097824ba5e8ebc049c38
Galiev Gleb <galievgleb99@gmail.com>
2022-10-12 15:49:27 +0300

    Update login_page.py

--- login_page.py
+++ login_page.py
@@ -3,7 +3,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class Login_page():
-
+ 
     def __init__(self, driver):
         self.driver = driver
 
